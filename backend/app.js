481901053f2a73c193ea24bbd4e0a6b02baa79d9
Janhavi-j001 <jadhavjanhavi001@gmail.com>
2025-05-09 01:02:08 +0530

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 5000;
 
+// Serve static files from the 'frontend' directory
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Define the route to serve the index.html file
 app.get('/', (req, res) => {
-    res.send('Inventory Management App Running-- updated message');
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html')); // Serve index.html
 });
 
 app.listen(PORT, () => {
